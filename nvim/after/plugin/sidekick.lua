b90c22b4c1033a03d7d75f4995565ec37921af04
Oliver Nelson <onelson@blizzard.com>
2026-04-01 17:21:43 +0200

    Update AICommit prompt to use staged changes

--- nvim/after/plugin/sidekick.lua
+++ nvim/after/plugin/sidekick.lua
@@ -165,7 +165,7 @@ end, {
 vim.api.nvim_create_user_command("AICommit", function()
   vim.api.nvim_cmd({
     cmd = "AIMessage",
-    args = { "exec `git commit -m`, create a commit message based on the changes." },
+    args = { "exec `git commit -m`, you decide the commit message based on the staged changes." },
   }, {})
 end, {
   desc = "Send git commit staged prompt to Sidekick",
